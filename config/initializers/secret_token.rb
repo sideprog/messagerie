2d1bcd4ed7cd124cbcd99ab1c047bb9f98bcdcea
Sideprog <sideprog1@gmail.com>
2012-12-31 16:28:10 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messagerie::Application.config.secret_token = 'fa86a4c2d180e510ccd5da8c71d0a4e7631b4de4b3ea0a21d18a03fa064f476c49a115fca7ec97e019a85aecd791efaaff34357ce078c9a5fb289d1288a5f2ea'
