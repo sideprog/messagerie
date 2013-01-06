2f4104199ac3cb81445c75e816624dd1aa067f12
Sideprog <sideprog1@gmail.com>
2013-01-06 18:23:14 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Messagerie::Application.routes.draw do
+  get "users/new"
   root to: 'static_pages#home'
+  match '/signup',  to: 'users#new'
+
+
 
 
   # The priority is based upon order of creation:
