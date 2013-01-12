50ab0079b844a079749accebfb0e35741114ef01
Sideprog <sideprog1@gmail.com>
2013-01-12 15:43:45 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Bienvenue!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 end
